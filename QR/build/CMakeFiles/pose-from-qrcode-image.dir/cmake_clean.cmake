c3c5fd8d1a360a6c01fc733c4da988953e57f7a5
Yong <wangyg1025@163.com>
2018-05-04 15:42:36 +0800

    first

---
+++ QR/build/CMakeFiles/pose-from-qrcode-image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose-from-qrcode-image.dir/pose-from-qrcode-image.cpp.o"
+  "pose-from-qrcode-image.pdb"
+  "pose-from-qrcode-image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose-from-qrcode-image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
